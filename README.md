131750429abe4273788270fc2e94a0c7de3b5479
GermainPinganaud <germain.pinganaud@gmail.com>
2019-01-08 14:48:57 +0100

    added the socket.io chat

---
+++ README.md
@@ -0,0 +1,23 @@
+## Principe du jeu
+
+Jeu de conquête spaciale.
+
+## Spécificités
+
+
+
+##TODO
+
+retirer le svg du html
+permettre la création dynamique de svg
+afficher un vaisseau en svg
+afficher une image par frame
+
+## Client
+
+- html
+- click
+- envoi
+
+## Server
+- variables (planet, vaisseau)
--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 window.onload = function() {
 
-class Circle {
+class Planet {
   constructor(centerX, centerY, radius, color) {
     this.centerX = centerX;
     this.centerY = centerY;
     this.radius = radius;
     this.color = color;
   }
+  svg = '<svg class="planet"></svg>'
+          <circle cx="100" cy="100" r="100" fill="white" />
+
+  display(){
+    html
+  }
 }
 
 let container = $(".container")
 
 
+
+setInterval(function(){ alert("Hello"); }, 3000);
+
+
 let homePlanet = $(".planet").children()
 
 console.log(homePlanet.attr("cx"));
-homePlanet.attr("cx", 1000)
+homePlanet.attr("cx", 100)
 console.log(homePlanet.attr("cx"));
 
-let planet = new Circle(100, 100, 10, "white")
+let planet = new Planet(100, 100, 10, "white")
 
 } // end of window.onload = function()
